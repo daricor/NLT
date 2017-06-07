5f646c02d484d21207943a9af22f5ab8c747411b
thibault <thibault_lascours@hotmail.com>
2017-06-07 15:30:25 +0200

    kkook

--- views/connexion.php
+++ views/connexion.php
@@ -1,8 +1,42 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Page: connexion.php
  */
+session_start(); // à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
+if (isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
+    // on vérifie que le champ "Pseudo" n'est pas vide
+    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
+    if (empty($_POST['username'])) {
+        echo "Le champ Identifiant est vide.";
+    } else {
+        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
+        if (empty($_POST['password'])) {
+            echo "Le champ Mot de passe est vide.";
+        } else {
+            // les champs sont bien posté et pas vide, on sécurise les données entrées par le membre:
+            $Username = htmlentities($_POST['username'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
+            $Password = htmlentities($_POST['password'], ENT_QUOTES, "ISO-8859-1");
+            //on se connecte à la base de données:
+            $mysqli = mysqli_connect("172.20.40.34", "root", "ROOTROOT", "newsletter");
+            //on vérifie que la connexion s'effectue correctement:
+            if (!$mysqli) {
+                echo "Erreur de connexion à la base de données.";
+            } else {
+                // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
+                $Requete = mysqli_query($mysqli, "SELECT * FROM User WHERE username = '" . $Username . "' AND password = '" . $Password . "'");
+                // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
+                // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
+                if (mysqli_num_rows($Requete) == 0) {
+                    echo "L'identifiant ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
+                } else {
+                    // on ouvre la session avec $_SESSION:
+                    $_SESSION['username'] = $Username; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
+                    echo "Vous êtes à présent connecté !";
+                }
+            }
+        }
+    }
+}
+?>
 
